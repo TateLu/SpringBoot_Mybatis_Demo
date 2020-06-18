98cffee3812f02aa09c2f408875613e79feee069
tanzey <ltz2you@163.com>
2020-06-18 14:59:59 +0800

    version 1.4

--- src/main/java/com/demo/controller/GlobalExceptionHandler.java
+++ src/main/java/com/demo/controller/GlobalExceptionHandler.java
@@ -1,4 +1,3 @@
-/*
 package com.demo.controller;
 
 import com.demo.entity.MyException;
@@ -12,24 +11,22 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
-*/
 /**
  * @Auther: cookie
  * @Date: 2018/7/26 15:09
  * @Description: 全局捕获异常和自定义全局捕获异常
- *//*
+ */
 
 @ControllerAdvice  //不指定包默认加了@Controller和@RestController都能控制
-//@ControllerAdvice(basePackages ="com.example.demo.controller")
 public class GlobalExceptionHandler {
 
-    */
 /**
      * 拦截捕捉自定义异常 MyException.class
      * @param myex
      * @return
-     *//*
+     */
 
+/*
     @ResponseBody
     @ExceptionHandler(value = RuntimeException.class)
     public Object myExceptionHandler(HttpServletRequest request, HttpServletResponse response, RuntimeException myex){
@@ -45,8 +42,8 @@ public class GlobalExceptionHandler {
 
 
     }
+*/
 
 
 
 }
-*/
